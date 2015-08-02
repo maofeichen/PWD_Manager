a6ffd3eacf19def1b8be92872d9f663f2b7bf160
mchen <mchen@debian.localhost>
2015-08-02 15:08:04 -0400

    Add gen_password()

--- pwd_manager.py
+++ pwd_manager.py
@@ -9,13 +9,40 @@ __author__    = "mchen"
 
 import getpass
 
+import hashlib
+import hmac
+
 def get_master_pwd():
     "Get master password from user, input from cmd."
     mas_pwd = getpass.getpass(prompt='Please input master password:\n')
     print(mas_pwd)
+    return mas_pwd
+
+def get_identity():
+    "Get the identity, the identitiy of the password."
+    identity = input("Please input the identity name:\n");
+    return identity
+
+def gen_password():
+    "Generate passwords based on the master password and identity."
+    b_mas_pwd = get_master_pwd().encode('utf-8')
+    b_identity = get_identity().encode('utf-8')
+    print("master password in binary: %s\n\
+            indentity in binary: %s"\
+            %(b_mas_pwd, b_identity) )
+  
+    mas_pwd_sha256 = hashlib.sha256(b_mas_pwd).digest()
+    print("master password in sha256 hash:\n%s\n\
+            lenth: %d"\
+            %(mas_pwd_sha256, len(mas_pwd_sha256) ) )
+
+    pwd = hmac.new(mas_pwd_sha256, \
+                    msg=b_identity, \
+                    digestmod=hashlib.sha256).hexdigest()
+    print("Password: %s"%(pwd) )
 
 def main():
-    get_master_pwd()
+    gen_password()
 
 if __name__ == "__main__":
     main()
